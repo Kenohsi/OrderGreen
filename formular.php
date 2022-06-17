8644ef3dbfdeb1da8860e5c94f7bb0631526d5b4
Kenohsi <91662939+Kenohsi@users.noreply.github.com>
2022-06-17 16:31:46 +0200

    Add files via upload

---
+++ formular.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE HTML!>
+<html>
+<head>
+
+	<title> Formular </title>
+	<meta name="author" content="Aldin">
+	<meta name="editor" content="notepad++">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="../php/format.css">
+	
+</head>
+
+<body> 
+
+	<h1> Formular </h1>
+	
+	<form action="formularauswertung.php" method="post">
+	
+	<input type="radio" name="sex" value="m">
+	Herr <br>
+	
+	<input type="radio" name="sex" value="f">
+	Frau<br>
+	
+	Ihr Name: <input type="text" name="nachname">
+	<input type="submit" value="absenden"><br>
+	
+	
+</form>
+	
+</body>
+
+</html>
